aee74deb084c9aca22391ab3f50f0a37bdbfc6c2
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-05-12 12:57:54 -0400

    scraping all flight elements

--- Lib/Scraper.py
+++ Lib/Scraper.py
@@ -1,26 +1,63 @@
 from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import requests
 
 import os
 
 class Scraper:
 
-    def __init__(self, price):
+    def __init__(self):
 
-        self.price = price
+        self.price = None
         self.flights = []
 
     def get_flights(self):
+        #In list below: Flight times, Airline, # of Stops, Layover Airport Code, Travel Duration, Price
+        # desired_class_names = ['vmXl vmXl-mod-variant-large','c_cgF c_cgF-mod-variant-default','JWEO-stops-text','c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','f8F1-price-text']
 
-        departure_location='JFK'
-        destination = 'ATH'
-        url="https://www.kayak.com/flights/{departure_location}-{self.destination}/2023-06-06?sort=bestflight_a".format(departure_location=departure_location, destination=destination)
-        doc = BeautifulSoup(requests.get(url).text,'html.parser')
+        driver = webdriver.Chrome()
 
-        # for flight in doc.select('.nrc6'):
-        for flight in range(0,6)
-            price = flight.select('.f8F1-price-text').text if flight.select('.f8F1-price-text') else ''
-            print(price)
+        url="https://www.kayak.com/flights/JFK-ATH/2023-06-06?sort=bestflight_a"
+        driver.get(url)
+        sleep(10)
+
+        # flights = driver.find_elements(By.CSS_SELECTOR, 'div.nrc6')
+        # print(flights, len(flights))
+        # for flight in flights:
+        #     price = flight.find_element(By.CSS_SELECTOR, 'div.f8F1-price-text')
+        #     print(price)
+        wait = WebDriverWait(driver, 10)
+        desired_class_names = ['vmXl vmXl-mod-variant-large','c_cgF c_cgF-mod-variant-default','JWEO-stops-text','c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','f8F1-price-text']
+        parent_elmnt_flights = driver.find_elements(By.XPATH, "//div[@class='nrc6']")
+        for parent_elmnt_flight in parent_elmnt_flights:
+            for class_name in desired_class_names:
+                # desired_elmnts = parent_elmnt_flight.find_elements(By.XPATH, f".//*[contains(@class, '{class_name}')]")
+                element = wait.until(EC.presence_of_element_located((By.XPATH, f"//div[@class='nrc6']//*[contains(@class, '{class_name}')]")))
+
+                for desired_elmnt in desired_elmnts:
+                    flight_details = {
+                        'Flight Time': desired_elmnt.text,
+                        'Airline': desired_elmnt.text,
+                        'Number of Stops': desired_elmnt.text,
+                        'Layover Airport Code': desired_elmnt.text,
+                        'Travel Duration': desired_elmnt.text,
+                        'Price': desired_elmnt.text
+                    }
+                    
+                    print(flight_details)
+            
+
+
+
+        
+
+
+s=Scraper()
+
+s.get_flights()
 
