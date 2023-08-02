9ffd0ddc10d78c569c31116c09d8a3ab84400335
zoe petropoulou <zoepetropoulou@zoes-MacBook-Air-3.local>
2023-08-02 19:12:31 +0300

    csv export

--- Lib/Flights.py
+++ Lib/Flights.py
@@ -1,5 +1,17 @@
+from Scraper import Scraper
 
-class Flights:
+def start_scraper():
+    departure_loc = input("Please enter your departure location: ")
+    destination = input("Please enter destination: ")
+    year = input("Enter year: ")
+    month = input("Enter month: ")
+    day = input("Enter day: ")
 
-    def __init__(self):
-        pass
+    s=Scraper(departure_loc, destination, year, month, day)
+    s.get_flights()
+
+def default_input_automatic_scraper(departure_loc='JFK', destination='ATH', year='2023', month='08', day='16'):
+    s=Scraper(departure_loc, destination, year, month, day)
+    s.get_flights()
+
+default_input_automatic_scraper()
--- Lib/Scraper.py
+++ Lib/Scraper.py
@@ -1,57 +1,132 @@
 from time import sleep
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
+import datetime
+import csv
 import requests
-
 import os
+import uuid
+import time
 
 class Scraper:
 
-    def __init__(self):
-
-        self.price = None
+    def __init__(self, departure_loc, destination, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.departure_loc = departure_loc
+        self.destination = destination
         self.flights = []
+
+    # def scroll_down(self, amount = 300):
+    #     driver = webdriver.Chrome()
+    #     driver.execute_script(f"window.scrollBY(0,{amount})")
+
+
 
     def get_flights(self):
         #In list below: Flight times, Airline, # of Stops, Layover Airport Code, Travel Duration, Price
         # desired_class_names = ['vmXl vmXl-mod-variant-large','c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','f8F1-price-text']
 
-        driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument("--incognito")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
 
-        url="https://www.kayak.com/flights/JFK-ATH/2023-06-06?sort=bestflight_a"
+
+        url=f'https://www.kayak.com/flights/{self.departure_loc}-{self.destination}/{self.year}-{self.month}-{self.day}?sort=bestflight_a'
         driver.get(url)
-        sleep(7)
+        sleep(5)
+        button_element = driver.find_element(By.XPATH, "//div[@role='button' and @class='ULvh-button show-more-button']")
+        button_element.click()
+        sleep(1)
 
-        desired_class_names = ['vmXl vmXl-mod-variant-large','c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','f8F1-price-text']
+        desired_class_names = ['vmXl vmXl-mod-variant-large', 'X3K_-segments', 'c_cgF c_cgF-mod-variant-default','vmXl vmXl-mod-variant-default','f8F1-price-text']
         parent_elmnt_flights = driver.find_elements(By.XPATH, "//div[@class='nrc6']")
 
-        all_element_contents = []
+        twoD_content = []
+        ff = []
 
-        for i in desired_class_names:
-            child_components = parent_elmnt_flights[0].find_elements(By.XPATH, f".//*[contains(@class, '{i}')]")
-            if child_components:
-                for cc in child_components:
-                    all_element_contents.append(cc.text)
-                    print(cc.text)
-                print('----------')
+        for trip in parent_elmnt_flights:
+            specific_element = trip.find_element(By.XPATH, ".//div[@class='nrc6-content-section' and @role='button']")
+
+            specific_element.click()
+            sleep(1.8)
+
+                # flight_num_elements = driver.find_elements(By.XPATH, "//div[@class='X3K_-segments']")
+
+                # for element in flight_num_elements:
+                #     flight_number = element.find_elements(By.XPATH, ".//div[@class='nAz5-carrier-text']")
+                #     for i in flight_number:
+                #         print(i.text)
+
+            all_element_contents = []
+
+            for i in desired_class_names:
+                child_components = trip.find_elements(By.XPATH, f".//*[contains(@class, '{i}')]")
+                if i == 'X3K_-segments':
+                    sleep(1)
+                    for element in child_components:
+                        flight_number = element.find_elements(By.XPATH, ".//div[@class='nAz5-carrier-text']")
+                        concatted_number = ''
+                        for j in flight_number:
+                            if j ==flight_number[-1]:
+                                concatted_number += j.text
+                            else:
+                                concatted_number += j.text + '\n'                        
+                        all_element_contents.append(concatted_number)
+                
+                elif i == 'c_cgF c_cgF-mod-variant-default':
+                    cgF = []
+                    iata_code_concat = ''
+                    for ii in child_components:
+                        if ii == child_components[-1]:
+                            iata_code_concat += ii.text
+                        else:
+                            iata_code_concat += ii.text + '\n'
+                    cgF.append(iata_code_concat)
+                    all_element_contents.append(cgF)
+
+                elif child_components:
+                    for cc in child_components:
+                        all_element_contents.append(cc.text)
+            
+            twoD_content.append(all_element_contents)
+            indices_of_interest = [0,1,2,3,4,5]
+            final_content = [all_element_contents[i] for i in indices_of_interest]
+            current_time = datetime.datetime.now()
+            formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+            final_content.append(formatted_time)
+            ff.append(final_content)
+
+        col_names = ['ID','Flight_Times', 'FLIGHT_NUMBER', 'Airline_Company_and_Layover_AIRprt','Number_of_stops','Flight_Duration','Price','Time_of_data_pull']
+
+        custom_folder_path = "/Users/zoepetropoulou/Development/code/post_flatiron"
+
+        current_id = 1
+        for row in ff:
+            row.insert(0, current_id)
+            current_id += 1
         
-        print(all_element_contents)
 
+        print(ff)
 
-
-                    
+        
             
 
 
+        csv_file_path = os.path.join(custom_folder_path, "testflights.csv")
+        with open(csv_file_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(col_names)
+            writer.writerows(ff)
 
-        
 
 
-s=Scraper()
 
-s.get_flights()
 
